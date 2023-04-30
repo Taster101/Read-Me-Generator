c1d0644cf70e5eadb3de1f96af9efee1d52786d7
Taster101 <Davontahenderson1@gmail.com>
2023-04-30 13:04:27 -0500

    base code for projesct

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,52 @@
-// TODO: Include packages needed for this application
+const  inquirer  = require("inquirer");
 
+// TODO: Include packages needed for this application
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{type:"input",
+                    message:'What is the the title of your project?',
+                    name:'title'
+                   } ,  
+                    {
+                    type:"input",
+                    message:'what is your project discription?',
+                    name:"discription"
+                   },
+                    {
+                    type:"input",
+                    message: 'Does your application attain a license?',
+                    name:'license',
+                    choices:['']
+                    
+                   },
+                   {
+                    type:"input",
+                    message:'What is your github username?',
+                    name:"github username"
+                   }, 
+                   {
+                   
+                   type:"input",
+                   message:'What is your email adress?',
+                   name: "email adress"
+                   }
+                   ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(file, data) {
+    fs.writeToFile('README.md',response,(err)=> {}
+    
+    
+    )
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data){
+        console.log(data)
+    })
+}
 
 // Function call to initialize app
 init();
